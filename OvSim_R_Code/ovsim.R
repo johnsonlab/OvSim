c471e470d4a080dc94c25f38cfa007bf8f6e331b
Josh Johnson <jjohnson17@gmail.com>
2016-02-24 15:06:33 -0500

    ovsim.R and papercode.R updated
    Codebase updated, added option for declining phold, instructions for
    user customization.

--- OvSim_R_Code/ovsim.R
+++ OvSim_R_Code/ovsim.R
@@ -4,6 +4,13 @@
 #' @param ND Number of 'days' or steps of the simulation.
 #' @param IGP Initial growing pool of follicles.
 #' @param phold Probability of 'holding' at the current number of cells.
+#' This is a fixed probability in the basic simulation, but can be customized.
+#' As an example, if `phold` is the character string 'custom1', the code
+#' shows a simple decline in `phold` from 0.995 down to 0.99 depending on
+#' the number of growth-activiated follicles at a given point in time being
+#' less than 100 (i.e. in older stages of the lifecycle). 
+#' Following this example, the `OvSim` function may be modified easily for
+#' other proposed variations of `phold`.
 #' @param cond.pdub Conditional probability of 'doubling', given that we
 #' didn't 'hold' initially.
 #' @param pcelllive The probability of a cell
@@ -11,9 +18,12 @@
 #' @param ejectnum After reaching this number of cells, a healthy follicle
 #' is ejected?
 #' @param cyclength The length of the ovulatory cycle in days.
-#' @param puberty Whether an initial 750 should have a head start.
-#' @param verbose Print the progress along the way?
-#' @param pdfname If a PDF file name is given, a plot will be produced.
+#' @param puberty Whether an initial `IGP` (300 by defualt)
+#' should have a head start.
+#' @examples
+#' ans <- ovsim()
+#' ans2 <- ovsim(phold="custom1")
+#' @export
 ovsim <- function(NF = 3000,
                   ND = 420,
                   IGP = 300,
@@ -22,9 +32,7 @@ ovsim <- function(NF = 3000,
                   pcelllive = 0.8,
                   ejectnum = 50000,
                   cyclength = 4,
-                  puberty = TRUE,
-                  verbose = TRUE,
-                  pdfname = NA)
+                  puberty = TRUE)
 {
   ## NOTE
   ##
@@ -34,16 +42,24 @@ ovsim <- function(NF = 3000,
   ## However, at the moment this serves to document the result of our
   ## paper. - Jay Emerson
   
-  # Initialize, randomly drawing 1, 2 or 3 cells
+  # Initialize, randomly drawing 1, 2 or 3 cells (uniformly for now)
   x <- sample(1:3, NF, replace=TRUE)
   
+  ### Want phold to possibly fall over time because fewer cells are in
+  ### the growth state (and producing AMH in this growth state).  An
+  ### example is added below.
+  
+  phold.orig <- phold
+  if (phold.orig=="custom1") phold <- 0.995
+  phold.new <- phold
   pdub <- (1-phold) * cond.pdub        # Unconditional doubling probability
   pdie <- (1-phold) * (1-cond.pdub)    # Unconditional death probability
-  
+
   # These are the unconditional transition probabilities at the start.
-  # JJJ: 2^16 is what gets us over the new 50,000 ejection number.
+  # JJJ: 2^16 is what gets us over the current 50,000 ejection number,
+  # for example.
   probs <- matrix(c(phold, pdub, pdie), NF, 3, byrow=TRUE)
-  JJJ <- 16
+  JJJ <- min(which(2^(1:25) > ejectnum))
   if (puberty) {
     x[1:IGP] <- 2^sample(1:JJJ, IGP, replace=TRUE,
                          dexp(1:JJJ, rate=0.3))
@@ -52,92 +68,59 @@ ovsim <- function(NF = 3000,
   }
   
   z <- matrix(0, NF, ND)
+  nga.byday <- rep(0, ND)
   for (i in 1:ND) {
-    if (verbose && (i/10 == floor(i/10))) cat("Step", i, "\n")
+    
+    if (i > 1) {
+      if (phold.orig=="custom1") {
+        # Here calculate phold based on the number growth-activated
+        # at i-1: sum(z[,i-1]>0 & probs[,i-1]==0)
+        val <- 0.99    # 0.995 for constant, 0.99 for adjusting based
+        # on the 0.995 upper value in this example customization
+        phold.new <- val + (0.995 - val) *
+          min(100, sum(z[,i-1]>0 & probs[,1]==0)) / 100
+      }
+      # The following only change if phold.new changed, above, but
+      # are needed anyway so I just recalculate here:
+      pdub.new <- (1-phold.new) * cond.pdub        # Unconditional doubling prob
+      pdie.new <- (1-phold.new) * (1-cond.pdub)    # Unconditional death prob
+    }
     for (j in 1:NF) {
+      if (i>1 && probs[j,1]!=0 && probs[j,1]!=1) {
+        probs[j,] <- c(phold.new, pdub.new, pdie.new) 
+      }
+      
       res <- sample(1:3, 1, prob=probs[j,]) # Status result
       
       # If "double" then we never "hold" in future steps and the probs change:
       if (res==2) { probs[j,] <- c(0, cond.pdub, 1-cond.pdub) }
       
       # If not "hold" then either double (2) or die (3)
-      
       if (res > 1) {
-        x[j] <- x[j] * 2 * (res==2)
+        x[j] <- x[j] * 2 * (res==2)   # i.e. res==3 is die trigger here
         if (x[j] > 0) x[j] <- rbinom(1, x[j], pcelllive)
       }
       
-      if (x[j] >= ejectnum) { probs[j,] <- c(1, 0, 0) } # Fol. ejected, stop
-    } # End of 1:NF loop
+      if (x[j] > ejectnum) {
+        probs[j,] <- c(1, 0, 0)
+        x[j] <- ejectnum
+      } # Fol. ejected, stop
+      
+    } # End of 1:NF loop (j)
     z[,i] <- x
-  } # End of 1:ND loop
+    nga.byday[i] <- sum(probs[,1]!=0 &
+                          (z[,i]==1 | z[,i]==2 | z[,i]==3))
+  } # End of 1:ND loop (i)
   
-  z[z>ejectnum] <- ejectnum
+  z[z>ejectnum] <- ejectnum  # Probably not necessary, but... can't hurt
   
-  if (verbose) {
-    cat("Dead follicles:\t\t", sum(z[,ncol(z)] == 0), "\n", sep="\t")
-    cat("Ejected eggs:\t", sum(z[,ncol(z)] == ejectnum), "\n", sep="\t")
-    cat("Follicles underdeveloped:", sum(z[,ncol(z)]>0 & z[,ncol(z)]<ejectnum),
-        "\n", sep="\t")
-  }
-  
-  wantfoo <- NULL
-  if (!is.na(pdfname)) {
-    pdf(pdfname, width=7.5, height=10, )
-    par(mfrow=c(3,1))
-    par(mar=c(5,7,4,2), ylbias=0.95)
-    
-    # For the paper, relies on extrapoints.csv and confsim.csv
-    # being available in the current directory
-    # ********************************
-    pfn <- read.csv("extrapoints.csv", as.is=TRUE)
-    b <- read.csv("confsim.csv", as.is=TRUE)
-    b <- apply(b, 2, quantile, probs=c(0.01, 0.99))
-    foo <- apply(z==1 | z==2 | z==3, 2, sum)
-    plot(1:ND, foo, xlab="Time (Days)",
-         ylab="Number of Primordial Follicles (1st and 99th Percentiles)",
-         pch="", ylim=range(pretty(c(0, foo))),
-         main="Decline of Primordial Follicle Pool")
-    polygon(c(1:ND, ND:1), c(b[1,], b[2,ND:1]), col="grey",
-            density=60)
-    points(pfn$days, pfn$pfn)
-    wantfoo <- foo
-    
-    # The ones that start to develop but die:     ****************
-    plot(1:ND, seq(1, 50000, length.out=ND), 
-         pch="", main="Growth of Follicles That Die Via Atresia",
-         xlab="Time (Days)", ylab="Number of Granulosa Cells",
-         las=1, yaxt="n")
-    axis(2, at=c(1, 1e4, 2e4, 3e4, 4e4, 5e4),
-         labels=format(c(1, 1e4, 2e4, 3e4, 4e4, 5e4), scientific=FALSE), las=1)
-    these <- which(apply(z==0, 1, any))
-    for (i in these) {
-      end <- max(1, min(which(z[i,]==0)) - 1)
-      lines(1:end, z[i,1:end], lty=3)
-      text(which.max(z[i,]), max(z[i,]), "D", cex=0.5)
-    }
-    
-    # Last one ****************************
-    foo <- apply(z==ejectnum, 1,
-                 function(a) {
-                   if (any(a)) return(min(which(a)))
-                   else return(NA)
-                 })
-    hist(foo, main="Distribution of Ovulatory Follicle Development",
-         xlab="Time (Days)", 
-         ylab=paste("Number of Ovulatory Follicles per", cyclength,
-                    "Day Estrus Cycle"),
-         breaks=seq(0, 420, by=cyclength))
-    abline(v=seq(1, 420, by=28), col="black", lty=2, lwd=1)
-    a <- table(cut(foo, breaks=seq(0, 420, by=cyclength)))
-    aa <- data.frame(a)
-    print(aa)    
-    
-    dev.off()
-  }
-  
-  return( list(all=z, dead=sum(z[,ncol(z)] == 0),
-               underdeveloped=sum(z[,ncol(z)]>0 & z[,ncol(z)]<ejectnum),
-               ejected=sum(z[,ncol(z)] == 0),
-               wantfoo=wantfoo) )
+  return( list(all=z,
+               dead.ND=sum(z[,ncol(z)] == 0),
+               nongrowing.ND=sum(z[,ncol(z)]>0 & z[,ncol(z)]<ejectnum & probs[,1]!=0),
+               growing.ND=sum(z[,ncol(z)]>0 & probs[,1]==0),
+               ejected.ND=sum(z[,ncol(z)] == ejectnum),
+               nongrowthactivated.byday=nga.byday,
+               NF=NF, ND=ND, IGP=IGP, phold=phold.orig, cond.pdub=cond.pdub,
+               pcelllive=pcelllive, puberty=puberty,
+               ejectnum=ejectnum, cyclength=cyclength) )
 }
